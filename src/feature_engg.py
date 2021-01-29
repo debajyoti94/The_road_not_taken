e00ea4b3c9e1f0a010c01766d71abd925bab5734
debajyoti <debajyoti94@gmail.com>
2021-01-29 17:41:33 +0530

    Added comments and created a basic template.

---
+++ src/feature_engg.py
@@ -0,0 +1,13 @@
+''' Here we preprocess the text which will be used for training the model'''
+
+# import modules here
+
+
+# tokenize the input sequences
+
+# tokenize the output sequences
+
+# load the glove vectors
+
+# create the embedding matrix
+
---
+++ src/test_functionalities.py
@@ -0,0 +1,16 @@
+''' Unit test cases for this project'''
+
+# import modules here
+
+
+# test if the file exists
+
+
+# test if the input and out have start and end sequences
+
+
+# test if the word embedding matrix is of expected shape
+
+
+# test if the word embeddings exists
+
