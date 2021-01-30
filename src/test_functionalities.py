accb80a20ca546de3fc7daa8659a1abea20075aa
debajyoti <debajyoti94@gmail.com>
2021-01-30 12:19:58 +0530

    Added comments for some test cases that should be added.

--- src/test_functionalities.py
+++ src/test_functionalities.py
@@ -1,9 +1,16 @@
 ''' Unit test cases for this project'''
 
 # import modules here
+import config
+import os
+import pickle
 
 
-# test if the file exists
+class TestFunctionalities:
+
+    # test if the file exists
+    def test_raw_file_exists(self):
+        assert True if os.path.isfile(config.RAW_DATASET) else False
 
 
 # test if the input and out have start and end sequences
