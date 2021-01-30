916afaa20077188877d38e75db2a1939552e4562
debajyoti <debajyoti94@gmail.com>
2021-01-30 14:52:03 +0530

    Added function for creating the embedding matrix.

--- src/feature_engg.py
+++ src/feature_engg.py
@@ -3,12 +3,34 @@
 # import modules here
 import config
 
+import abc
 import numpy as np
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-class DataPreprocessing:
+
+class MustHaveDP:
+
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def tokenize_input(self):
+        '''
+        For tokenizing input and output data
+        :return: tokenized data
+        '''
+        return
+
+    @abc.abstractmethod
+    def create_embedding_matrix(self):
+        '''
+        For creating the embedding matrix for fixed size input
+        :return: embedding matrix
+        '''
+        return
+
+class DataPreprocessing(MustHaveDP):
 
     # tokenize the input/output
     def tokenize_input(self, dataset):
@@ -43,6 +65,19 @@ class DataPreprocessing:
         input_sequences = tokenizer.texts_to_sequences(input_text)
         output_sequences = tokenizer.texts_to_sequences(output_text)
 
+        # now we need to create input and output sequences of fixed length
+        max_sequence_len = max(len(s) for s in input_sequences)
+        max_sequence_len = min(max_sequence_len,
+                               config.MAX_SEQUENCE_LENGTH)
+        input_sequences = pad_sequences(input_sequences,
+                                        maxlen=max_sequence_len,
+                                        padding='post')
+        output_sequences = pad_sequences(output_sequences,
+                                        maxlen=max_sequence_len,
+                                        padding='post')
+        # post indicates we will be
+        # padding at the end of the sentence
+
         return input_sequences, output_sequences, tokenizer
 
     def load_glove_vectors(self, pretrained_model):
@@ -62,7 +97,30 @@ class DataPreprocessing:
 
         return word_vector_dict
 
-# load the glove vectors
 
-# create the embedding matrix
+    def create_embedding_matrix(self, tokenizer, word_vector_dict):
+        '''
+
+        :param tokenizer:
+        :param word_vector_dict:
+        :return:
+        '''
+
+        # here we will create the word embedding matrix
+        # basically, we will get the matrix of words and vectors
+        # which exist in the voacbulary of the dataset
+
+        word2idx = tokenizer.word_index
+        num_words = min(config.MAX_VOCAB_SIZE, len(word2idx)+1) # +1 bcoz the index in tokenizer starts at 1
+        embedding_matrix = np.zeros(shape=(num_words, config.EMBEDDING_DIM))
+
+        for word, index in word2idx.items():
+            try:
+                embedding_matrix[index] = word_vector_dict.get(word)
+            except KeyError:
+                continue
+
+        return embedding_matrix
+
+
 
