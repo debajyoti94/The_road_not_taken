5ada29c0e2e88bdc28d5b561fee549f8df1ee1ae
debajyoti <debajyoti94@gmail.com>
2021-01-31 16:24:10 +0530

    Created template for training and testing the model.

---
+++ src/model.py
@@ -0,0 +1,19 @@
+''' In this code we will define the model for training and testing'''
+
+import config
+
+from keras.models import Model
+from keras.layers import LSTM, Input
+from keras.optimizers import Adam
+
+class LanguageModel:
+
+    def train_model(self):
+        return
+
+
+    def inference_stage(self):
+        return
+
+    def plot_values(self, model):
+        return
