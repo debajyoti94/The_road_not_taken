2fe09fc1f162766326919516128921b3c58bf856
debajyoti <debajyoti94@gmail.com>
2021-02-02 18:35:14 +0530

    Added code for unit test cases.

--- src/test_functionalities.py
+++ src/test_functionalities.py
@@ -4,6 +4,7 @@
 import config
 import os
 import pickle
+import feature_engg
 
 
 class TestFunctionalities:
@@ -12,11 +13,23 @@ class TestFunctionalities:
     def test_raw_file_exists(self):
         assert True if os.path.isfile(config.RAW_DATASET) else False
 
+    # test if the word embeddings exists
+    def test_pretrained_embedding_exists(self):
+        assert True if os.path.isfile(config.PRETRAINED_EMBEDDINGS)\
+            else False
 
-# test if the input and out have start and end sequences
+    # test if the input and output have start and end sequences
+    def test_input_output_sequences(self):
+        dl_obj = feature_engg.DumpLoadFile()
+        tokenizer = dl_obj.load_file(config.TOKENIZER)
+        assert True if tokenizer[0].word_index['<sos>'] and\
+                       tokenizer[0].word_index['<eos>'] \
+            else False
+
+    # test if the word embedding matrix is of expected shape
+    def test_embedding_matrix_shape(self):
+        dl_obj = feature_engg.DumpLoadFile()
+        embedding_matrix = dl_obj.load_file(config.EMBEDDING_MATRIX)
+        assert True if embedding_matrix[0].shape[1] == config.EMBEDDING_DIM else False
 
 
-# test if the word embedding matrix is of expected shape
-
-
-# test if the word embeddings exists
